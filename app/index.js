7903e62756afe48bc14c9b9d310a5f3a1385f718
Jill Cates <cates.jill@gmail.com>
2017-02-17 08:20:11 -0800

    Add routing

--- app/index.js
+++ app/index.js
@@ -1,16 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div> Hello World !!!</div>
-    )
-  }
-});
+var routes = require('./config/routes');
 
 
 ReactDOM.render(
-  <HelloWorld />,
+  routes,
   document.getElementById('app')
 );
