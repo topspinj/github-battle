f4f1629e91e2461c8a848b04f95290174060aa94
Jill Cates <cates.jill@gmail.com>
2017-03-09 17:15:48 -0800

    Finish the github battle confirm page, update css

--- app/styles/index.js
+++ app/styles/index.js
@@ -1,7 +1,15 @@
 var styles = {
   transparentBg: {
     background: 'transparent'
+  },
+  space: {
+    marginTop: '25px',
+  },
+  box_dimensions: {
+    height: '300px',
+    width: '300px',
+    margin: '0 auto',
   }
-};
+}
 
 module.exports = styles;
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -1,24 +1,24 @@
 var axios = require('axios');
 
-// var id = "YOUR_CLIENT_ID";
-// var sec = "YOUR SECRET_ID";
-// var param = "?client_id=" + id + "&client_secret=" + sec;
+var client_id = "b69b122d2e824fc91f9e";
+var secret_id = "f205cc68552a23ec8f015d41e8c81c517c93a8fa";
+var param = "?client_id=" + client_id + "&client_secret=" + secret_id;
 
 function getUserInfo (username) {
-  return axios.get('https://api.github.com/users/' + username) //param
+  return axios.get('https://api.github.com/users/' + username + param);
 }
 
 var helpers = {
-  getPlayersInfo: function(players) {
-    return axios.all(players.map(function(username){
+  getPlayersInfo: function (players) {
+    return axios.all(players.map(function (username) {
       return getUserInfo(username)
-    })).then(function(info){
-      return info.map(function(user){
-        return user.data;
+    }))
+    .then(function (info) {
+      return info.map(function (user) {
+        return user.data
       })
-    }).catch(function(err) {
-      console.warn('Error in getPlayersInfo', err)
     })
+    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
